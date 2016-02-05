b91cb5bbca1150a110d469106d8fc7f474a2a111
NickGargov <nick.gargov@gmail.com>
2016-02-05 00:41:19 -0800

    I have added the add groups module and modified it to have you enter the group name and password along with error checks for both

--- CSE110TaskList/app/src/main/java/com/example/stevenzheng/cse110tasklist/AddGroup.java
+++ CSE110TaskList/app/src/main/java/com/example/stevenzheng/cse110tasklist/AddGroup.java
@@ -2,15 +2,89 @@ package com.example.stevenzheng.cse110tasklist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import org.json.JSONArray;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 /**
  * Created by stevenzheng on 2/1/16.
  */
 public class AddGroup extends Activity {
 
+    private EditText groupCodeView;
+    private EditText groupNameView;
+    String groupCodeText;
+    String groupNameText;
+    ParseUser currentUser;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_group);
+        currentUser = ParseUser.getCurrentUser();
+    }
+
+    public void onButtonClick(View v) {
+
+        if( v.getId() == R.id.B_joinGroup) {
+            groupCodeView = (EditText) findViewById(R.id.TextField_groupPassword);
+            groupNameView = (EditText) findViewById(R.id.TextField_findtaskListName);
+            groupCodeText = groupCodeView.getText().toString();
+            groupNameText = groupNameView.getText().toString();
+
+            //Create a query to get the first object that matches the ID of password
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("Group");
+            query.whereEqualTo("password", groupCodeText);
+            query.getFirstInBackground(new GetCallback<ParseObject>() {
+                @Override
+                public void done(ParseObject object, ParseException e) {
+                    //If the object is found, the password was correct
+                    if(e == null){
+                        //Test whether or not the group name is now valid
+                        ParseQuery<ParseObject> query2 = ParseQuery.getQuery("Group");
+                        query2.whereEqualTo("name", groupNameText);
+                        query2.getFirstInBackground(new GetCallback<ParseObject>() {
+                            @Override
+                            public void done(ParseObject object, ParseException e) {
+                                if (e == null) {
+                                    ArrayList<ParseObject> groups = new ArrayList<ParseObject>();
+                                    groups = (ArrayList<ParseObject>) ((List<ParseObject>) (Object) (currentUser.getList("groupsList")));
+                                    // Append the new group and update current user with their new group list
+                                    object.add("members", currentUser.getString("firstName"));
+                                    groups.add(object);
+                                    currentUser.put("groupsList", groups);
+                                    currentUser.saveInBackground();
+
+                                    // Refresh main menu and finish
+                                    setResult(RESULT_OK, null);
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(getApplicationContext(), "Group name or Password is incorrect", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Group name or Password is incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
--- CSE110TaskList/app/src/main/java/com/example/stevenzheng/cse110tasklist/CreateGroup.java
+++ CSE110TaskList/app/src/main/java/com/example/stevenzheng/cse110tasklist/CreateGroup.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import org.json.JSONArray;
 import java.util.List;
 
+import com.parse.GetCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 /**
@@ -45,28 +49,44 @@ public class CreateGroup extends Activity {
             groupNameTxt = groupNameView.getText().toString();
             groupPasswordTxt = groupPasswordView.getText().toString();
 
-            //Group group = new Group(groupNameTxt, groupPasswordTxt);
-            // Create the new group
-            ParseObject newGroup =  new ParseObject("Group");
-            ArrayList<ParseUser> members = new ArrayList<ParseUser>();
-            members.add(currentUser);
-            newGroup.put("name", groupNameTxt);
-            newGroup.put("password", groupPasswordTxt);
-            newGroup.put("members", members);
-            newGroup.saveInBackground();
+            //Check if there is already a group with that unique password
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("Group");
+            query.whereEqualTo("password", groupPasswordTxt);
+            query.getFirstInBackground(new GetCallback<ParseObject>() {
+                @Override
+                public void done(ParseObject object, ParseException e) {
+                    //If the object is found then tell user to enter new password
+                    if(e == null){
+                        Toast.makeText(getApplicationContext(), "Password already exists, make new password", Toast.LENGTH_SHORT).show();
+                    }
+                    //The object was not found so that means make it
+                    else{
+                        //Group group = new Group(groupNameTxt, groupPasswordTxt);
+                        // Create the new group
+                        ParseObject newGroup =  new ParseObject("Group");
+                        ArrayList<ParseUser> members = new ArrayList<ParseUser>();
+                        members.add(currentUser);
+                        newGroup.put("name", groupNameTxt);
+                        newGroup.put("password", groupPasswordTxt);
+                        newGroup.put("members", members);
+                        newGroup.saveInBackground();
 
-            // Get user's current groups
-            ArrayList<ParseObject> groups = new ArrayList<ParseObject>();
-            groups = (ArrayList<ParseObject>)((List<ParseObject>) (Object) (currentUser.getList("groupsList")));
+                        // Get user's current groups
+                        ArrayList<ParseObject> groups = new ArrayList<ParseObject>();
+                        groups = (ArrayList<ParseObject>)((List<ParseObject>) (Object) (currentUser.getList("groupsList")));
 
-            // Append the new group and update current user with their new group list
-            groups.add(newGroup);
-            currentUser.put("groupsList", groups);
-            currentUser.saveInBackground();
+                        // Append the new group and update current user with their new group list
+                        groups.add(newGroup);
+                        currentUser.put("groupsList", groups);
+                        currentUser.saveInBackground();
 
-            // Refresh main menu and finish
-            setResult(RESULT_OK, null);
-            finish();
+                        // Refresh main menu and finish
+                        setResult(RESULT_OK, null);
+                        finish();
+                    }
+                }
+            });
+
         }
     }
 }
