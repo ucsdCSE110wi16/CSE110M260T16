0c86ebff20a6d4ebbe77bf6f58bfae0005a31dcc
Abhishek Kumar <abhishek.kumar.1115@gmail.com>
2016-02-06 22:24:54 -0800

    set up espresso ui testing

---
+++ CSE110TaskList/app/src/androidTest/java/com/example/stevenzheng/cse110tasklist/SignInTest.java
@@ -0,0 +1,121 @@
+package com.example.stevenzheng.cse110tasklist;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static java.lang.Thread.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+// Turn all animation scales to off before running tests
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SignInTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    private MainActivity mActivity = null;
+
+
+    @Before
+    public void setActivity() {
+        mActivity = mActivityRule.getActivity();
+    }
+
+    public void signIn() {
+        onView(withId(R.id.B_signInForm)).perform(click());
+        String testPass = "qwerty";
+        String testEmail = "ak@ak.com";
+
+        // sign in with test credentials
+        onView(withId(R.id.TextField_email)).perform(typeText(testEmail));
+        onView(withId(R.id.TextField_password)).perform(typeText(testPass));
+
+        // click button to sign in
+        onView(withId(R.id.B_signIn)).perform(click());
+    }
+
+    // Given that I have an account
+    // When I enter my credentials and log in
+    // Then I should see a list of task lists
+    @Test
+    public void testSignIn() throws InterruptedException {
+        onView(withId(R.id.B_signInForm)).perform(click());
+        String fName = "a";
+        String testPass = "qwerty";
+        String testEmail = "ak@ak.com";
+
+        // sign in with test credentials
+        onView(withId(R.id.TextField_email)).perform(typeText(testEmail));
+        onView(withId(R.id.TextField_password)).perform(typeText(testPass));
+
+        // click button to sign in
+        onView(withId(R.id.B_signIn)).perform(click());
+        // TODO: find non temporary solution to time delay
+        sleep(4000);
+        onView(withId(R.id.Text_name)).check(matches(withText(fName)));
+    }
+
+    // stubbed out create list testing
+//    @Test
+//    public void testCreateList() throws InterruptedException {
+//        signIn();
+//        String str = "Create Task List";
+////         click button to sign in
+//        onView(withId(R.id.B_createNewList)).perform(click());
+////         TODO: find non temporary solution to time delay
+//        sleep(2000);
+//        onView(withId(R.id.textView3)).check(matches(withText(str)));
+//    }
+
+//    // stubbed out sign up testing
+//    @Test
+//    public void testSignUp() throws InterruptedException {
+//        onView(withId(R.id.B_signUp)).perform(click());
+//        String fName = "FName tester";
+//        String lName = "lName tester";
+//        String testPass = "password";
+//        String testEmail = "testing@test.com";
+//
+////         enter name
+//        onView(withId(R.id.TextField_firstName)).perform(typeText(fName));
+//        onView(withId(R.id.TextField_lastName)).perform(typeText(lName));
+////         sign in with test credentials
+//        onView(withId(R.id.TextField_email)).perform(typeText(testEmail));
+//        onView(withId(R.id.TextField_password)).perform(typeText(testPass));
+//
+////         click button to sign in
+//        onView(withId(R.id.B_signIn)).perform(click());
+////         TODO: find non temporary solution to time delay
+//        sleep(2000);
+//        onView(withId(R.id.Text_name)).check(matches(withText(fName)));
+//    }
+
+}
