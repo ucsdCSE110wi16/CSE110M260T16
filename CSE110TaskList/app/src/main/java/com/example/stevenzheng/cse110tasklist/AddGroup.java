bbf4e193d7b99c481fa0e905c8d8eec1f9dc4f63
Steven Zheng <szzheng@ucsd.edu>
2016-03-07 11:52:02 -0800

    Fix add list bug

--- CSE110TaskList/app/src/main/java/com/example/stevenzheng/cse110tasklist/AddGroup.java
+++ CSE110TaskList/app/src/main/java/com/example/stevenzheng/cse110tasklist/AddGroup.java
@@ -35,7 +35,7 @@ public class AddGroup extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_group_temp);
+        setContentView(R.layout.add_group);
         currentUser = ParseUser.getCurrentUser();
     }
 
